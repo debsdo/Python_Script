962744c9d7099d5ed660527e914e50c1c8e38b2c
debsdo <48097561+debsdo@users.noreply.github.com>
2019-05-29 10:54:00 +0530

    Add files via upload
    This Python script moves specific files from one folder to another.

---
+++ move_sp_files.py
@@ -0,0 +1,33 @@
+import shutil, os
+
+file = ["20161207-111327-0.jpg",
+"20170718-095159-2.jpg",
+"20170811-133439-1.jpg",
+"20170811-133752-1.jpg",
+"20170217-114015-0.jpg",
+"20170811-123145-2.jpg",
+"20170811-131023-1.jpg",
+"20170405-155147-0.jpg",
+"20161207-111246-0.jpg",
+"20170718-102020-2.jpg",
+"20170217-103427-0.jpg",
+"20170811-134228-1.jpg",
+"20170217-151259-0.jpg",
+"20170811-112723-2.jpg",
+"20170210-131822-0.jpg",
+"20170718-095128-2.jpg",
+"20170811-125149-2.jpg"]
+
+	 	
+
+dir_src = 'D:\\deepweedsx\\DeepWeeds_Images_256'
+
+dir_dst = 'D:\\project\\train' 
+
+#dir_dst = 'D:\\project\\test'
+
+for f in file:
+    	print file  # testing
+   	src_file = os.path.join(dir_src, f)
+	dst_file = os.path.join(dir_dst, f)
+    	shutil.move(src_file, dst_file)
